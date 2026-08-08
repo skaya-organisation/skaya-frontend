c582c0b3cc59a41dd977cd83d5bca14ab1313e9b
shubhamkunwar10 <fonders@metakul.com>
2026-08-08 15:13:50 +0530

    update home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,10 @@
 import { useEffect, useState } from "react";
 import { Hero, Features, HowItWorks, Showcase, FAQ, CTA, Footer, AIShowcase, ContactFormDialog, Products } from "../components/SaaS";
-import { ClerkProvider } from "@clerk/clerk-react";
 import "./index.css";
 import { useDark } from 'rspress/runtime';
-import { dark } from "@clerk/themes";
-import { PUBLISHABLE_KEY } from "../utils/constants";
 import { PortfolioShowcase } from "../components/PortfolioShowcase";
 import { Scales } from "../components/ui/Scales";
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Add your Clerk Publishable Key to the .env file");
-}
 
 export default function HomePage() {
   const darkMode = useDark();
@@ -24,12 +18,7 @@ export default function HomePage() {
   }, []);
 
   return (
-    <ClerkProvider
-      publishableKey={PUBLISHABLE_KEY}
-      appearance={{
-        baseTheme: darkMode === true ? dark : undefined,
-      }}
-    >
+
       <div className={`overflow-hidden transition-colors duration-300 relative`}>
         {/* Left Scales Border */}
         <div className="absolute left-0 top-0 bottom-0 w-6 lg:flex items-center justify-start z-40 group/left">
@@ -67,6 +56,5 @@ export default function HomePage() {
           darkMode={darkMode}
         />
       </div>
-    </ClerkProvider>
   );
 }
